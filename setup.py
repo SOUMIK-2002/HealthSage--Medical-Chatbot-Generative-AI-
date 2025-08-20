1bb26ecc9c97cb6d946da907e6d005f34711195d
Soumik Modak <soumikmodak17@gmail.com>
2025-08-20 13:37:46 +0530

    requirements.added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="HealthSage_chatbot",
+    version="0.1.0",
+    author="Soumik Modak",
+    author_email="soumikmodak17@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
